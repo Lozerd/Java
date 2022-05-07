07c915f3e1264946b241b7209f55c5dd890bc995
lozerd <dmisha29411@gmail.com>
2022-05-07 18:38:42 +0300

    init project

---
+++ src/Labs/JavaClass.java
@@ -0,0 +1,35 @@
+package Labs;
+
+public class JavaClass {
+    private static class Solution {
+        private String customName;
+        private Boolean newLine;
+        public Solution(String customName, Boolean newLine){
+            this.customName = customName;
+            this.newLine = newLine;
+        }
+
+        public void SayHelloToName() {
+            if (!this.newLine) {
+                System.out.print(this.customName);
+            } else {
+                System.out.println(this.customName);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution("Mikhail", false);
+
+        s.SayHelloToName();
+        s.customName = "Roman";
+        s.newLine = true;
+        s.SayHelloToName();
+
+        System.out.print("Hello World!");
+        // Output will be MikhailRoman\nHello World!
+    }
+
+
+}
